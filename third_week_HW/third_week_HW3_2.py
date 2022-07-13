19d08d19449795b4d66aed74b50e614b68cfec63
alexei_96@inbox.ru <alexei_96@inbox.ru>
2022-07-13 12:03:41 +0300

    resolved the formula of waiting.

--- third_week_HW/third_week_HW3_2.py
+++ third_week_HW/third_week_HW3_2.py
@@ -5,9 +5,10 @@ def decorator_params(call_count=2, start_sleep_time=5, factor=2, border_sleep_ti
         def wrapper(*args):
             print(f"Количество запусков = call_count({call_count}).")
             print("Начало работы.")
-            # Начальное время ожидания.
-            t = start_sleep_time
-            for exp in range(1,call_count + 1):
+
+            for exp in range(0,call_count):
+                # Время ожидания запуска функции
+                t = start_sleep_time * factor ** exp
                 if t < border_sleep_time:
                     time.sleep(t)
                 else:
@@ -15,17 +16,12 @@ def decorator_params(call_count=2, start_sleep_time=5, factor=2, border_sleep_ti
                     time.sleep(t)
                 print(f"Запуск номер {exp}. Ожидание {t} секунд. Результат работы декорируемой функции =", end=" ")
                 value = func()
-                """ 
-                Увеличиваем время ожидания после каждой итерации, до тех пор пока оно не превысит 
-                border_sleep_time.
-                """
-                t *= 2**factor
             print("Конец работы.")
             return value
         return wrapper
     return decorator
 
-@decorator_params(call_count=5, start_sleep_time=1)
+@decorator_params(call_count=5, start_sleep_time=1, factor=2, border_sleep_time=20)
 def function():
     print("Функция работает.")
 
