a7bf25dcc873db973a21465d7bb3a60e8cb8ae69
alexei_96@inbox.ru <alexei_96@inbox.ru>
2022-07-13 11:48:13 +0300

    Now if the parameter is in cash, the cashed function is not called

--- third_week_HW/third_week_HW3_1.py
+++ third_week_HW/third_week_HW3_1.py
@@ -3,17 +3,16 @@ def cashing_decorator(func) -> int:
     # Создаем словарик(кэш), куда будем складывать результаты работы функции.
     cash = {}
     def wrapper(*args):
-        # Результат выполнения функции "multiplier".
-        result = func(*args)
         # Проверяем, есть ли такой результат функции в кэше.
         if args in cash:
             print(f"Если наш декоратор работает и значение параметра({args[0]}) есть в кэше,"
-                  f"то эта строка выведится на экран")
+                  f"то эта строка выведится на экран.")
             return cash[args]
-        else:
-            print(f"Такого параметра({args[0]}) функции multiplier нет в кэше, поэтому записываем его туда")
-            cash[args] = result
-            return result
+        print(f"Такого параметра({args[0]}) функции multiplier нет в кэше, поэтому записываем его туда.")
+        # Результат выполнения функции "multiplier".
+        result = func(*args)
+        cash[args] = result
+        return result
     return wrapper
 
 @cashing_decorator
@@ -25,5 +24,4 @@ if __name__ == "__main__":
     # Создадим список, куда положим несколько значений, чтобы проверить работу нашего декоратора.
     checking = [5, 5, 10, 10]
     for num in checking:
-        print(multiplier(num))
-
+        print("Результат работы функции:", multiplier(num))
